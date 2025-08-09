18e3ca729c79f24f6e7fdf133a65d256836eecf6
Mohamed <mosalah3480@gmail.com>
2025-08-08 23:20:54 -0500

    Make the resume builder script remove the temp files, create a base prompt for tailoring resumes

--- build_resume.sh
+++ build_resume.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-output=$(pdflatex resume.tex 2>&1)
+resume_file="resume"
+
+output=$(pdflatex $resume_file.tex 2>&1)
 status=$?
 
 if [ $status -ne 0 ]; then
     echo "$output"
 fi
+
+rm -f $resume_file.aux $resume_file.log $resume_file.out
 
 exit $status
